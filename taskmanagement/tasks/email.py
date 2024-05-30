833d18fef9649720e2b159b9f9f70df7ff48d726
sherry22616804 <162660152+sherry22616804@users.noreply.github.com>
2024-05-30 23:08:53 +0800

    Update email.py

--- taskmanagement/tasks/email.py
+++ taskmanagement/tasks/email.py
@@ -1,6 +1,7 @@
 from django.core.mail import EmailMessage
 from django.conf import settings
 from django.template.loader import render_to_string
+from .userinfo import CustomUser
 
 
 class SendEmail:
@@ -9,7 +10,7 @@ class SendEmail:
 
     def __init__(self,emailadd = None):
         self.EMAIL_HOST_USER = settings.EMAIL_HOST_USER
-        self.emailadd  = self.EmailAdd 
+        self.emailadd  = '' 
 
     # def set_email(email):
     #      = email
@@ -32,9 +33,22 @@ class SendEmail:
         # self.EmailAdd = user_email
         
         self.emailadd = user_email
+        SendEmail.EmailAdd = user_email
+
+    def getemail(self,username):
+        user = CustomUser.objects.get(username=username)
+        user_email = user.email
+        SendEmail.EmailAdd = user_email
+        # self.emailadd = user_email
 
 
     def reminder(self):
+
+        # user_email = CustomUser.email.__get__
+        # user = CustomUser.objects.get(username=username)
+        # user = CustomUser.objects.get
+        # user_email = user.email
+
         email_template = render_to_string(
                 'signup_success_email.html',
                 {'username': 'abcnd'}
@@ -44,12 +58,18 @@ class SendEmail:
                 '註冊成功通知信2',  # 電子郵件標題
                 email_template,  # 電子郵件內容
                 settings.EMAIL_HOST_USER,  # 寄件者
-                [self.emailadd]  # 收件者
+                [SendEmail.EmailAdd]  # 收件者
             )
         email.fail_silently = False
         email.send()
+        # print(SendEmail.EmailAdd)
         # self.EmailAdd = user_email
         # # self.emailadd = user_email
         # print(self.emailadd)
 
     
+
+    def corn_job():
+        print("This is cronjob test")
+
+
